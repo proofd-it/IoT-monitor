a913fe520aa5bde8b634e09f848b0376446dee16
devon96 <kdryja@gmail.com>
2019-12-09 21:07:29 +0000

    add various fixes

--- scanner.js
+++ scanner.js
@@ -1,39 +1,50 @@
+const DEV = false;
+
 const USE_PROBE = false;
-const SCAN_FREQ = 5 * 60000;
+const TIME = null;
+
+if (DEV) {
+  const SCAN_FREQ = 5000;
+} else {
+  const SCAN_FREQ = 5 * 60000;
+}
+if (TIME) {
+  setTime(TIME);
+}
 
 function onInit() {
   // Watch for reset button press. More than 3 seconds will initiate tearDown.
-  NRF.setAdvertising({}, {name : "logger"});
-  setWatch(function() {
+  NRF.setAdvertising({}, {name: "logger"});
+  setWatch(function () {
     var cancel = false;
     var led = false;
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
       led = !led;
       digitalWrite(LED2, !led ? 1 : 0);
     }, 200);
     // Cancel if button released within 3 seconds.
-    setWatch(function() {
+    setWatch(function () {
       cancel = true;
       digitalWrite(LED2, 0);
       clearInterval(interval);
-    }, BTN, {edge : "falling", debounce : 50, repeat : false});
+    }, BTN, {edge: "falling", debounce: 50, repeat: false});
 
-    setTimeout(function() {
+    setTimeout(function () {
       if (!cancel) {
         tearDown();
         digitalWrite(LED2, 0);
         clearInterval(interval);
       }
     }, 5000);
-  }, BTN, {edge : "rising", debounce : 50, repeat : true});
+  }, BTN, {edge: "rising", debounce: 50, repeat: true});
 
-  var scanInterval = setInterval(function() { logState(); }, SCAN_FREQ);
+  var scanInterval = setInterval(function () {logState();}, SCAN_FREQ);
 }
 
 function tearDown() {
   // Light blue LED for confirmation.
   digitalWrite(LED3, 1);
-  setTimeout(function() {
+  setTimeout(function () {
     digitalWrite(LED3, 0);
     // Remove all existing logs.
     var f = require("Storage");
@@ -58,18 +69,19 @@ function logState(s) {
   var f = require("Storage");
   var name = Math.ceil(getTime()) % 100000000;
 
-  var probe;
+  var probe = null;
   if (USE_PROBE) {
     var ow = new OneWire(D1);
     probe = readProbe();
     console.log("probe temp is: " + probe);
   }
   f.write(name, JSON.stringify({
-    d : Math.ceil(getTime()),
-    t_sensor : E.getTemperature(),
-    t_probe : probe,
-    b : Puck.getBatteryPercentage()
+    d: Math.ceil(getTime()),
+    t_sensor: E.getTemperature(),
+    t_probe: probe,
+    b: Puck.getBatteryPercentage()
   }));
+  console.log(f.read(name));
 }
 
 function readProbe() {
@@ -96,6 +108,13 @@ function getNames() {
 function getReading(name) {
   var f = require("Storage");
   return f.read(name);
+}
+
+function getAll() {
+  var names = getNames();
+  names.forEach(function(item) {
+    console.log(getReading(item));
+  });
 }
 
 onInit();
